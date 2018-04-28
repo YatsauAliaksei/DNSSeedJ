0b2c6c307a5db8e33a067819a75dfabdc21a6e95
ayatsau <ayatsau@cisco.com>
2018-04-28 15:32:21 +0200

    Tor socks proxy support

--- src/main/java/by/mrj/SimpleSeed.java
+++ src/main/java/by/mrj/SimpleSeed.java
@@ -5,6 +5,7 @@ import by.mrj.message.domain.Registration;
 import by.mrj.message.types.Command;
 import by.mrj.message.util.MessageUtils;
 import by.mrj.message.util.NetUtils;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.IOException;
 import java.io.OutputStream;
@@ -12,39 +13,66 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 /**
  * Simplest possible implementation of "DNS seed".
  * NOTE: actually not a DNS seed :)
  */
+@Log4j2
 public class SimpleSeed {
+
+    static {
+        System.setProperty("socksProxyHost", "127.0.0.1");
+        System.setProperty("socksProxyPort", "9050");
+        System.setProperty("http.proxyHost", "127.0.0.1");
+        System.setProperty("http.proxyPort", "9050");
+        System.setProperty("https.proxyHost", "127.0.0.1");
+        System.setProperty("https.proxyPort", "9050");
+    }
 
     public static void main(String[] args) {
         Set<String> addresses = new HashSet<>();
-        try (ServerSocket ss = new ServerSocket(53)) {
+        try (ServerSocket ss = new ServerSocket(8353)) {
             while (true) {
                 // receive request
+                log.info("Listening on default port...");
                 Socket socket = ss.accept();
-                @SuppressWarnings("unchecked")
-                Message<Registration> message = (Message<Registration>) NetUtils.deserialize(socket.getInputStream());
 
-                processMessage(message);
+                log.info("Accepted request");
 
-                String address = message.getAddress();
-                addresses.add(address);
+                CompletableFuture.runAsync(() -> {
+                    try (Socket sockX = socket) {
+                        @SuppressWarnings("unchecked")
+                        Message<Registration> message = (Message<Registration>) NetUtils.deserialize(sockX.getInputStream());
 
-                OutputStream os = socket.getOutputStream();
+                        processMessage(message);
 
-                String peers = addresses.stream().reduce((s, s2) -> s + ";" + s2).orElse("");
+                        String address = message.getPayload().getNetworkAddress();
+                        log.info("Got peer network address [{}]", address);
 
-                Message<Registration> registrationMessage = MessageUtils.makeMessageWithPubKey(
-                        Registration.builder().address(peers).build(),
-                        Command.REGISTRATION);
+                        if (address == null || address.trim().length() < 7) // todo: weird check...
+                            return;
 
-                byte[] bytes = NetUtils.serialize(registrationMessage);
-                os.write(bytes);
-                os.flush();
-                socket.close();
+                        addresses.add(address);
+                        String peers = addresses.stream().collect(Collectors.joining(";"));
+
+                        Message<Registration> registrationMessage = MessageUtils.makeMessageWithSig(
+                                Registration.builder().networkAddress(peers).build(),
+                                Command.REGISTRATION);
+
+                        byte[] bytes = NetUtils.serialize(registrationMessage);
+                        OutputStream os = sockX.getOutputStream();
+                        os.write(bytes);
+                        os.flush();
+//                        sockX.shutdownOutput();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).exceptionally(throwable -> {
+                    throw new RuntimeException(throwable);
+                });
             }
         } catch (IOException e) {
             e.printStackTrace();
